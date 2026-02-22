db5050ffe742630361d834c2bcc86dddc42f3bc9
nightvoyager111 <xianghanzzz@outlook.com>
2026-02-22 13:12:24 +0800

    Modified deliver

--- lab3/deliver.c
+++ lab3/deliver.c
@@ -8,7 +8,8 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <time.h>
-#include <errno.h>
+#include <sys/time.h>
+#include <math.h>
 
 #define DATA_SIZE 1000
 #define BUF_SIZE 4096
@@ -35,6 +36,13 @@ int main(int argc, char *argv[]) {
 
     int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
+    // RTT estimation variables
+    double estimated_rtt = 0.1;
+    double dev_rtt = 0.0;
+    double timeout_interval = 0.5;
+    const double alpha = 0.125, beta = 0.25;
+
+
     fseek(fp, 0, SEEK_END); //move fp to the end of file
     long filesize = ftell(fp); //size of file
     rewind(fp);//move fp to start of file
@@ -48,83 +56,49 @@ int main(int argc, char *argv[]) {
     struct timespec sendtime, recvtime; //time logic
     // double expected_rtt = 0.5;
 
-    // For section 3
-    // RTO variables 
-    double srtt = -1.0;
-    double rttvar = 0.0;
-    double rto = 1.0; // Initial RTO = 1 second before any RTT measurements
+    for (unsigned int frag_no = 1; frag_no <= total_frag; frag_no++) {
+        size_t size = fread(data, 1, DATA_SIZE, fp);
+        int header_len = snprintf(packet, sizeof(packet), "%u:%u:%zu:%s:", total_frag, frag_no, size, filename);
+        memcpy(packet + header_len, data, size);
+            
 
-    for (unsigned int frag_no = 1; frag_no <= total_frag; ) {
-        static int data_loaded = 0; // flag
-        static size_t size = 0;
-        static int header_len = 0;
+        while (1) { 
+            struct timeval tv;
+            tv.tv_sec = (long)timeout_interval;
+            tv.tv_usec = (long)((timeout_interval - tv.tv_sec) * 1e6);
+            setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
 
-        if (!data_loaded) {
-            size = fread(data, 1, DATA_SIZE, fp); //read up to 1000Bytes 
-            header_len = snprintf(packet, sizeof(packet),
-                                 "%u:%u:%zu:%s:", total_frag, frag_no, size, filename);
-            memcpy(packet + header_len, data, size);
-            data_loaded = 1;
-        }
-
-        struct timeval tv;
-        tv.tv_sec = (time_t)rto;
-        tv.tv_usec = (suseconds_t)((rto - tv.tv_sec));
-        setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-
-        clock_gettime (CLOCK_MONOTONIC, &sendtime); //
-        sendto(sockfd, packet, header_len + size, 0, res->ai_addr, res->ai_addrlen);
-
-        while (1) { //why do we need this loop
-            struct sockaddr_storage server;
-
-            socklen_t server_len = sizeof(server);
-
-            ssize_t rn = recvfrom(sockfd, ctrlbuf, sizeof(ctrlbuf)-1, 0, (struct sockaddr *)&server, &server_len);
+            clock_gettime (CLOCK_MONOTONIC, &sendtime); //
+            sendto(sockfd, packet, header_len + size, 0, res->ai_addr, res->ai_addrlen);
+            
+            struct sockaddr_storage server_addr;
+            socklen_t server_len = sizeof(server_addr);
+            ssize_t rn = recvfrom(sockfd, ctrlbuf, sizeof(ctrlbuf)-1, 0, (struct sockaddr *)&server_addr, &server_len);
             
             if (rn < 0) {
-                if (errno == EAGAIN || errno == EWOULDBLOCK) {
-                    printf("Timeout on fragment %u, retransmit\n", frag_no);
-                    data_loaded = 1; 
-                    continue;
-                }
-                perror("recvfrom"); // other errors
-                break;
+                printf("Timeout. Retransmitting fragment %u\n", frag_no);
+                timeout_interval *= 1.5;
+                continue;
             }
             
             ctrlbuf[rn] = '\0'; 
             clock_gettime(CLOCK_MONOTONIC, &recvtime);
             double rtt = (recvtime.tv_sec - sendtime.tv_sec) + (recvtime.tv_nsec - sendtime.tv_nsec)/1e9;
 
-            char type[8];
-            unsigned int fno;
+            estimated_rtt = (1 - alpha) * estimated_rtt + alpha * rtt;
+            dev_rtt = (1 - beta) * dev_rtt + beta * fabs(rtt - estimated_rtt);
+            timeout_interval = estimated_rtt + 4 * dev_rtt;
+            if (timeout_interval < 0.01) timeout_interval = 0.1;
 
-            if (sscanf(ctrlbuf, "%7[^:]:%u", type, &fno) != 2) continue;
-            if (fno != frag_no) continue;
+            unsigned int ack_no;
+            if (sscanf(ctrlbuf, "ACK: %u", &ack_no) == 1 && ack_no == frag_no) {
+                printf("Fragment %u sent. RTT: %f s\n", frag_no, rtt);
+                break;
+            } else {
+                printf("Received NACK or wrong ACK. Retransmitting fragment %u\n", frag_no);
+            }
 
-            if (strcmp(type, "NACK") == 0) {
-                printf("NACK received for fragment %u, retransmi\n", frag_no);
-                data_loaded = 1;
-                continue;
-            }
-                    
-            if (strcmp(type, "ACK") == 0) {
-                // update SRTT, RTTVAR, RTO using TCP formula
-                if (srtt < 0) {
-                    //first measurement
-                    srtt = rtt;
-                    rttvar = rtt / 2.0; 
-                } else {
-                    double alpha = 0.875, beta = 0.75;
-                    rttvar = beta * rttvar + (1-beta) * fabs(srtt - rtt);
-                    srtt = alpha * srtt + (1-alpha) * rtt;
-                }
-                rto = srtt + 4 * rttvar;
-                if (rto < 0.1) rto = 0.1;
-                printf("Fragment %u sent. RTT: %f s, RTO: %f s\n", frag_no, rtt, rto);
-                frag_no++;
-                data_loaded = 0;
-            }
+           
         }
     }
 
